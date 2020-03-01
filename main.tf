04beb4b020526cb5f985e03619a8b4c4b6430eb2
Lorenzo Canese <lorenzocanese@gmail.com>
2020-03-01 18:37:32 +0100

    API Gateway integration

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ data "archive_file" "zip" {
   output_path = "aws-lambda-go.zip"
 }
 
-resource "aws_lambda_function" "hello" {
-  function_name    = "hello"
+resource "aws_lambda_function" "time" {
+  function_name    = "time"
   filename         = "aws-lambda-go.zip"
   handler          = "aws-lambda-go"
   source_code_hash = "data.archive_file.zip.output_base64sha256"
@@ -33,4 +33,51 @@ resource "aws_iam_role" "iam_for_lambda" {
   ]
 }
 EOF
+}
+
+resource "aws_api_gateway_rest_api" "api" {
+  name = "time_api"
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  path_part   = "time"
+  parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+  resource_id   = "${aws_api_gateway_resource.resource.id}"
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
+  resource_id             = "${aws_api_gateway_resource.resource.id}"
+  http_method             = "${aws_api_gateway_method.method.http_method}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.time.invoke_arn}"
+}
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.time.function_name}"
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
+}
+
+resource "aws_api_gateway_deployment" "time_deploy" {
+  depends_on = [aws_api_gateway_integration.integration]
+
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  stage_name  = "v1"
+
+}
+
+output "url" {
+  value = "${aws_api_gateway_deployment.time_deploy.invoke_url}${aws_api_gateway_resource.resource.path}"
 }
