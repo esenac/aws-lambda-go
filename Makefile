8aefc035ac9e37d051782fe7e5cfd0b919fff92e
esenac <lorenzocanese@gmail.com>
2022-02-13 15:49:03 +0100

    Update Go version and Terraform syntax

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ build:
 	docker build -t esenac/aws-lambda-go .
 
 compile: 
-	docker run -e GOOS=linux -e GOARCH=amd64 -v $$(pwd):/app -w /app golang:1.13 go build -ldflags="-s -w" -o bin/aws-lambda-go
+	docker run -e GOOS=linux -e GOARCH=amd64 -v $$(pwd):/app -w /app golang:1.16 go build -ldflags="-s -w" -o bin/aws-lambda-go
 
 plan: compile
 	docker run -v $$(pwd)/main.tf:/srv/main.tf -v $$(pwd)/terraform.tfstate:/srv/terraform.tfstate -v $$(pwd)/bin:/srv/bin -e AWS_ACCESS_KEY_ID=$$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$$AWS_DEFAULT_REGION esenac/terraform-aws plan
--- main.tf
+++ main.tf
@@ -41,30 +41,30 @@ resource "aws_api_gateway_rest_api" "api" {
 
 resource "aws_api_gateway_resource" "resource" {
   path_part   = "time"
-  parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
-  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.api.id
 }
 
 resource "aws_api_gateway_method" "method" {
-  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
-  resource_id   = "${aws_api_gateway_resource.resource.id}"
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.resource.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "integration" {
-  rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
-  resource_id             = "${aws_api_gateway_resource.resource.id}"
-  http_method             = "${aws_api_gateway_method.method.http_method}"
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.time.invoke_arn}"
+  uri                     = aws_lambda_function.time.invoke_arn
 }
 
 resource "aws_lambda_permission" "apigw_lambda" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.time.function_name}"
+  function_name = aws_lambda_function.time.function_name
   principal     = "apigateway.amazonaws.com"
 
   source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
@@ -73,7 +73,7 @@ resource "aws_lambda_permission" "apigw_lambda" {
 resource "aws_api_gateway_deployment" "time_deploy" {
   depends_on = [aws_api_gateway_integration.integration]
 
-  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  rest_api_id = aws_api_gateway_rest_api.api.id
   stage_name  = "v1"
 
 }
